06617ff863bdf8e6d193b7447e2506a53a68a412
vinutagithub <vinuta_ktk@yahoo.co.in>
2020-10-26 23:06:04 -0700

    Initial commit - project structure created

---
+++ src/main/java/org/Calculator_class.java
@@ -0,0 +1,26 @@
+package org;
+
+public class Calculator_class {
+
+    int a;
+    int b;
+    int sum;
+    int sub;
+    int mul;
+
+    public void Calculator_class(int a, int b){
+
+        sum(a,b);
+        sub(a,b);
+        mul(a,b);
+    }
+
+    private void mul(int a, int b) {
+    }
+
+    private void sub(int a, int b) {
+    }
+
+    private void sum(int a, int b) {
+    }
+}
---
+++ src/test/java/org/Calculator_Test.java
@@ -0,0 +1,21 @@
+package org;
+
+import org.testng.annotations.Test;
+
+public class Calculator_Test {
+
+    @Test
+    public void tc_add(){
+
+    }
+
+    @Test
+    public void tc_sub(){
+
+    }
+
+    @Test
+    public void tc_mul(){
+
+    }
+}
